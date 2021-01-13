1e651ba653208d231008aaa63a52d3b457dc0f0e
andrelohmann <lohmann.andre@gmail.com>
2021-01-13 21:43:39 +0100

    updated Adder to resize the fontsize regarding the text length

--- src/Smartformer/ImageTextOverlay/Adder.php
+++ src/Smartformer/ImageTextOverlay/Adder.php
@@ -9,31 +9,32 @@ class Adder {
      */
     private $fontPath;
 
-    function __construct($font){
+    function __construct($fontPath){
         $this->fontPath = $fontPath;
     }
 
     /**
      * Add text to JPEG
      */
-    public function addTextToJpeg($text, $img_path, $output, $r = 0, $g = 0, $b = 0, $font_size = 70, $angle = 0) {
+    public function addTextToJpeg($text, $img_path, $output, $padding = 15, $r = 0, $g = 0, $b = 0, $font_size = 70, $angle = 0) {
         $our_image = imagecreatefromjpeg($img_path);
         $color = imagecolorallocate($our_image, $r, $g, $b);
         $image_info = getimagesize($img_path);
-        $left = $image_info[0] / 2;
-        $top = $image_info[1] / 2;
         
+        /* Create text */
         $text_box = imagettfbbox($font_size, $angle, $this->fontPath, $text);
-        $text_width = $text_box[2]-$text_box[0];
-        $text_height = $text_box[7]-$text_box[1];
-        $left = ($image_info[0] / 2) - ($text_width/2);
+        $text_width = $text_box[2] - $text_box[0];
+        $text_height = $text_box[7] - $text_box[1];     
+        while(($text_width > ($image_info[0] - $padding))) {
+            $font_size--;
+            $text_box = imagettfbbox($font_size, $angle, $this->fontPath, $text);
+            $text_width = $text_box[2] - $text_box[0];
+            $text_height = $text_box[7] - $text_box[1];     
+        }
         $top = ($image_info[1] / 2) - ($text_height/2);
+        $left = ($image_info[0] / 2) - ($text_width/2);
 
-        // Shadow
-        $grey = imagecolorallocate($our_image, 128, 128, 128);
-        imagettftext($our_image, $font_size, $angle, $left, $top+1, $grey, $this->font, $text);
-        
-        imagettftext($our_image, $font_size, $angle, $left, $top, $color, $this->font, $text);
+        imagettftext($our_image, $font_size, $angle, $left, $top, $color, $this->fontPath, $text);
         imagejpeg($our_image, $output);
         imagedestroy($our_image);
     }
@@ -41,25 +42,30 @@ class Adder {
     /**
      * Add text to PNG
      */
-    public function addTextToPng($text, $img_path, $output, $r = 0, $g = 0, $b = 0, $font_size = 70, $angle = 0) {
+    public function addTextToPng($text, $img_path, $output, $padding = 15, $r = 0, $g = 0, $b = 0, $font_size = 70, $angle = 0) {
         $our_image = imagecreatefrompng($img_path);
         $color = imagecolorallocate($our_image, $r, $g, $b);
+        imageAlphaBlending($our_image, true);
+        imageSaveAlpha($our_image, true);
         $image_info = getimagesize($img_path);
-        $left = $image_info[0] / 2;
-        $top = $image_info[1] / 2;
         
+        /* Create text */
         $text_box = imagettfbbox($font_size, $angle, $this->fontPath, $text);
-        $text_width = $text_box[2]-$text_box[0];
-        $text_height = $text_box[7]-$text_box[1];
-        $left = ($image_info[0] / 2) - ($text_width/2);
+        $text_width = $text_box[2] - $text_box[0];
+        $text_height = $text_box[7] - $text_box[1];     
+        while(($text_width > ($image_info[0] - $padding))) {
+            $font_size--;
+            $text_box = imagettfbbox($font_size, $angle, $this->fontPath, $text);
+            $text_width = $text_box[2] - $text_box[0];
+            $text_height = $text_box[7] - $text_box[1];     
+        }
         $top = ($image_info[1] / 2) - ($text_height/2);
+        $left = ($image_info[0] / 2) - ($text_width/2);
 
-        // Shadow
-        $grey = imagecolorallocate($our_image, 128, 128, 128);
-        imagettftext($our_image, $font_size, $angle, $left, $top+1, $grey, $this->font, $text);
-        
-        imagettftext($our_image, $font_size, $angle, $left, $top, $color, $this->font, $text);
+        imagettftext($our_image, $font_size, $angle, $left, $top, $color, $this->fontPath, $text);
         imagepng($our_image, $output);
         imagedestroy($our_image);
     }
+
+    
 }
